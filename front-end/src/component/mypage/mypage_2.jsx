db689c78a4178a9261c4ffb2b6d66ccd6f18356f
ddogong <yeon020615@naver.com>
2023-07-27 18:29:35 +0900

    마이페이지 이용방법 링크 추가

--- front-end/src/component/mypage/mypage_2.jsx
+++ front-end/src/component/mypage/mypage_2.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 const Mypage_2 = () => {
+    const handleClick1 = () => {
+        window.open('https://www.notion.so/Loadstar-010b97456e40412489fcd9d46c926521?pvs=4');
+    };
     return (
         <div className="mypage_2_container">
-
+            <div className="mypage_2_manual_loadstar">"이용방법을 확인하고 LoadStar의 다양한 기능을 사용해보세요!"</div>
+            <div className="mypage_2_manual_container_img"><img className="manual_img" src={require("../image/manual.png")}></img></div>
+            <div className="mypage_2_manual_container">
+            <div onClick={handleClick1}>이용방법 보기</div>
+            </div>
         </div>
     );
 };
