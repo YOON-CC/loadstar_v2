c872d88b74b0c980e59afbb62593d922fb62663c
ddogong <yeon020615@naver.com>
2023-08-15 21:28:14 +0900

    마이페이지 수정

--- front-end/src/component/mypage/mypage_5.jsx
+++ front-end/src/component/mypage/mypage_5.jsx
@@ -3,9 +3,21 @@ import "./mypage.css";
 import { Link } from "react-router-dom";
 
 const Mypage_5 = () => {
+  const handleManualClick = () => {
+    window.open(
+      "https://www.notion.so/6fe66bc44fa2491cbd8c94675bd17cef?v=c1de34f3715c462e926079e0b9fb7b97&pvs=4"
+    );
+  };
+
   return (
     <div className="mypage_container">
-      <div className="mypage_top"></div>
+      <div className="mypage_6_top">
+        loadstar는 보다 자세한 이용 방법을 안내해드리기 위해 아래 링크를
+        제공합니다.
+      </div>
+      <button className="mypage_6_content" onClick={handleManualClick}>
+        loadstar 이용 방법 바로가기
+      </button>
     </div>
   );
 };
