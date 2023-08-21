ef6608cceb1f9202168574d8fa8a1db031d969e7
ddogong <yeon020615@naver.com>
2023-08-21 22:27:15 +0900

    마이페이지 내가 쓴 게시글, 내가 쓴 댓글, 북마크한 글 ui 수정

--- front-end/src/component/mypage/mypage_4.jsx
+++ front-end/src/component/mypage/mypage_4.jsx
@@ -7,6 +7,29 @@ const Mypage_4 = () => {
   return (
     <div className="mypage_container">
       <div className="mypage_top">내가 북마크한 게시글만 모았어요 !</div>
+      <div className="mypage_4_container">
+        <div className="mypage_4_content">
+          <div className="mypage_4_content_title">깃허브 사용방법</div>
+          <div className="mypage_4_content_info">
+            <div className="mypage_4_content_info_1">
+              <div className="mypage_4_content_username">강서연</div>
+              <div className="mypage_4_content_date">2023-08-20</div>
+              <div className="mypage_4_content_bookview">
+                <div className="mypage_4_content_bookmark">
+                  <img src={require("../image/star.png")}></img>
+                  <div>99+</div>
+                </div>
+                <div className="mypage_4_content_view">
+                  <img src={require("../image/check.png")}></img>
+                  <div>99+</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mypage_4_content"></div>
+        <div className="mypage_4_content"></div>
+      </div>
     </div>
   );
 };
